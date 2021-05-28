a22a20bb5cfa1ff6e691010bee0f89603b9fbbb6
ian-yitzhak <onyangoian71@gmail.com>
2021-05-28 12:09:00 +0300

    Modified mongodb password

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    "mongodb+srv://ian_test:<>@ian.qfm7u.mongodb.net/dbname?retryWrites=true&w=majority", 
+    "mongodb+srv://ian_test:<password>@ian.qfm7u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
     { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true  }
 )
  .then(console.log('connected succesfully'))
